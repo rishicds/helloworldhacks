8a012c1de0aea81f059e1c80a4bcdedf02ac1af3
rishicds <rishi.404@outlook.com>
2025-03-16 19:33:40 +0530

    chore: Update @microsoft/clarity dependency to version 1.0.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,45 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 import "./globals.css";
-
+import { ReactNode } from "react";
 import Footer from "@/components/shared/Footer";
 import LoadingWrapper from "@/components/shared/LoaderClient";
 import CoolHeader from "@/components/shared/Header";
 import MusicButton from "@/components/shared/MusicButton";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "HELLOWORLDHACKS",
   description: "A hackathon for everyone",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <LoadingWrapper>
-          <Analytics/>
-          <CoolHeader/>
-          {children}
-          <MusicButton />
-          <Footer />
-        </LoadingWrapper>
+    <>
+      <Script
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "qp5ayba370");
+          `,
+        }}
+      />
+      <html>
+      <body>
+      <LoadingWrapper>
+      <CoolHeader />
+      <main>{children}</main>
+      <Footer />
+      <MusicButton />
+      <Analytics />
+      </LoadingWrapper>
       </body>
-    </html>
+      </html>
+    </>
   );
 }
