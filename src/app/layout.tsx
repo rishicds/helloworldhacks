11e90808f9a75f5650e65778150ea0f11fb3c313
rishicds <rishi.404@outlook.com>
2025-03-16 14:32:37 +0530

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 import Footer from "@/components/shared/Footer";
@@ -33,6 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <LoadingWrapper>
+          <Analytics/>
           <CoolHeader/>
           {children}
           <MusicButton />
