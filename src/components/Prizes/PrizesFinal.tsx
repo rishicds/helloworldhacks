5d7bcc0c1f58b9c460664d485282002f47d915f5
Rishi <124495375+rishicds@users.noreply.github.com>
2025-03-21 20:15:23 +0530

    Update PrizesFinal.tsx

--- src/components/Prizes/PrizesFinal.tsx
+++ src/components/Prizes/PrizesFinal.tsx
@@ -251,7 +251,7 @@ export default function PrizesFinal() {
 
   useEffect(() => {
     const checkMobile = () => {
-      setIsMobile(window.innerWidth < 768)
+      setIsMobile(window.innerWidth < 1024)
     }
 
     checkMobile()
